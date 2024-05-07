e6d33db3253300ac315a050610e2b2dc45344357
Nario <oliwka1416@gmail.com>
2024-05-06 23:51:01 -0700

    FINISHED FINITO

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -19,3 +19,27 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+// swiper for the project pages
+
+const swiper1 = new Swiper('.swiper-project', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
